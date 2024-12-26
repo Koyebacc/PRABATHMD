f1fc43066e66707735552b57e066077cadffe604
Koyebacc <gohoket831@chansd.com>
2024-12-26 18:05:09 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94713457207"
+const SESSION_ID = "PRABATH-MD~ZJhiFT5I#i-vzrbtikttm7w-pWE5zvcvYxSb0KfkV-BP7__wVuBc"
 
 
 module.exports = {
